c0939744e9d7b23846346e095f6e84e44a3807ef
marilia-borgo <marilia.bm01@gmail.com>
2022-06-29 13:50:33 -0300

    secret key escondida

--- ola_mundo/settings.py
+++ ola_mundo/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!$k58k#a*a4ida5(*%g@6*k8o^4!z@yqpgltj433ez7_%g8kwz'
+SECRET_KEY = os.getenv('SECRET_KEY','secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
